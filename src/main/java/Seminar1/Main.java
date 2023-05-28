ab0f4b651f8538a9713f1d8290b8bbd9b2d09b6a
Einzel <einzel0102@mail.ru>
2023-05-28 12:51:07 +0800

    "Киоск по продажи товаров"

---
+++ src/main/java/Seminar1/Main.java
@@ -0,0 +1,29 @@
+package Seminar1;
+
+import Seminar1.Cheese;
+import Seminar1.Chocolate;
+
+public class Main {
+    public static void main(String[] args) {
+        VendingMachine apparat = new VendingMachine();
+        apparat.addProduct(new Chocolate("Alpen gold", 10, "темный",
+                        "с орехом", 100))
+                .addProduct(new Product("яблоко", 30))
+                .addProduct(new Product("Кола", 15))
+                .addProduct(new Cheese("Российский", 10, "вкусный", 1000));
+
+
+            Product saledProduct = apparat.saleProduct("яблоко");
+            System.out.println(saledProduct);
+            Product saledProduct2 = apparat.saleProduct("Кола");
+            System.out.println(saledProduct2);
+
+            System.out.println("__________________________________________");
+
+            System.out.println(apparat);
+
+
+    }
+}
+
+
